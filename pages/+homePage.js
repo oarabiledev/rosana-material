e9e2c6d720f6186868d3d1abec04cfd4f5ce1198
oarabilekoore42 <oarabilekoore42@gmail.com>
2024-11-15 09:00:15 +0000

    simple project set-up

--- pages/+homePage.js
+++ pages/+homePage.js
@@ -2,4 +2,5 @@ import { $LinearLayout, $Html } from 'rosana';
 
 let homePage = $LinearLayout('fillxy, vcenter');
 
-export homePage;
+$Html.H1(homePage).batch({textContent: "Hello World"})
+export default homePage;
